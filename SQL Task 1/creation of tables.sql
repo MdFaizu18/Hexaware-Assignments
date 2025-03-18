cf34c0f4e2ebf5c1ab9e092f42db8ac8e05e23a8
Mohammed Faizulla <136733703+MdFaizu18@users.noreply.github.com>
2025-03-18 10:12:55 +0530

    Update creation of tables.sql

--- SQL Task 1/creation of tables.sql
+++ SQL Task 1/creation of tables.sql
@@ -1,22 +1,28 @@
+-- creation of database
 CREATE DATABASE HMBank;
 USE HMBank;
--- DROP TABLE customers;
+
+-- to create the customers table
 CREATE TABLE customers(
-      customer_id INT PRIMARY KEY,
+      customer_id INT PRIMARY KEY AUTO_INCREMENT,
       first_name VARCHAR(50),
       last_name VARCHAR(50),
       DOB DATE NOT NULL,
       email VARCHAR(100) NOT NULL UNIQUE,
-      phone_number INT NOT NULL,
+      phone_number VARCHAR(15) NOT NULL,
       address VARCHAR(150)
 );
+
+-- to create the accounts table
 CREATE TABLE accounts(
-      account_id INT PRIMARY KEY,
+      account_id INT PRIMARY KEY AUTO_INCREMENT,
       customer_id INT,
       account_type VARCHAR(20) CHECK (account_type IN ('SAVINGS', 'CURRENT', 'ZERO_BALANCE')),
 	  balance DECIMAL(10,2) NOT NULL DEFAULT 0.00,
       FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
 );	
+
+-- to create the transactions table
 CREATE TABLE transactions(
       transaction_id INT PRIMARY KEY AUTO_INCREMENT,
 	  account_id INT,
@@ -25,4 +31,6 @@ CREATE TABLE transactions(
       transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
      FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE
 );
+
+-- to show all tables in database
 SHOW TABLES;
