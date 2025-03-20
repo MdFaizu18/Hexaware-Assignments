315b299a7383ad1e232a6b9b70626f7798749569
Mohammed Faizulla <136733703+MdFaizu18@users.noreply.github.com>
2025-03-20 21:07:22 +0530

    Add files via upload

---
+++ Java Task 3/CalculateCompoundInterest.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CalculateCompoundInterest {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("\nEnter the No of Customers: ");
+    int numberOfCustomers = sc.nextInt();
+
+    for(int i =0;i<numberOfCustomers;i++){
+        System.out.println("\nCustomer " + (i+1) + ": ");
+
+        System.out.print("Please Enter the Initial Balance: $ ");
+        double initialBalance = sc.nextDouble();
+        if(initialBalance <=0){
+            System.out.println("Initial Amount Should be Greater than 0");
+            System.exit(0);
+        }
+
+        System.out.print("Please Enter Annual Interest Rate: % ");
+        double annualInterestRate = sc.nextDouble();
+        if(annualInterestRate <=0){
+            System.out.println("Annual Interest Should be Greater than 0");
+            System.exit(0);
+        }
+
+        System.out.print("Please Enter the Number of Years: ");
+        int numberOfYears = sc.nextInt();
+        if(numberOfYears <=0){
+            System.out.println("Number of Years Should be Greater than 0");
+            System.exit(0);
+        }
+
+        double futureBalance  = initialBalance * Math.pow((1 + annualInterestRate/100),numberOfYears);
+        System.out.printf("Future Balance after %d years: $ %.2f\n", numberOfYears, futureBalance);
+    }
+    sc.close();
+}
+}
