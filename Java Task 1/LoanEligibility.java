1cf530b4b1f477d286fc6fb01938ae26e4d5d9da
Mohammed Faizulla <136733703+MdFaizu18@users.noreply.github.com>
2025-03-21 11:39:51 +0530

    Done Java Task 1

--- Java Task 1/LoanEligibility.java
+++ Java Task 1/LoanEligibility.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 public class LoanEligibility {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        //To enter the credit score
         System.out.println("Enter your credit score: ");
         int creditScore = sc.nextInt();
+        //TO enter the annual income
         System.out.println("Enter your annual income: ");
         int annualIncome = sc.nextInt();
+        //Checking for eligibility for the loan
         if (creditScore > 700 && annualIncome >= 50000) {
             System.out.println("Congratulations! You are eligible for the loan.");
         } else {
