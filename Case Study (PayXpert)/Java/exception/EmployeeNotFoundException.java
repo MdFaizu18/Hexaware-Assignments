71848db9a32c5cd90e27b62fd91f8ac706fbd77b
Mohammed Faizulla <136733703+MdFaizu18@users.noreply.github.com>
2025-04-09 16:05:24 +0530

    Delete Case Study (PayXpert)/Java directory

--- Case Study (PayXpert)/Java/exception/EmployeeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.payxpert.exception;
-
-
-
-public class EmployeeNotFoundException extends Exception {
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-}
